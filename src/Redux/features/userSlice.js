fd42e2e242f1e89bb37134643a3fd00727e7b094
Lê Minh Thắng <107106191+lethangd@users.noreply.github.com>
2025-01-04 16:45:33 +0700

    fx

--- src/Redux/features/userSlice.js
+++ src/Redux/features/userSlice.js
@@ -17,11 +17,12 @@ const userSlice = createSlice({
       // Lưu token vào cookie bằng js-cookie
       Cookies.set("token", token, {
         path: "/",
-        domain: "localhost",
-        secure: true,
-        sameSite: "None",
-        expires: 7,
-      }); // Cookie hết hạn sau 7 ngày
+        // Sử dụng biến môi trường từ file .env để cấu hình domain
+        domain: process.env.REACT_APP_DOMAIN || "localhost", // Nếu không có giá trị trong .env, sẽ dùng "localhost"
+        secure: process.env.NODE_ENV === "production", // Bật secure khi ở production (HTTPS)
+        sameSite: "None", // Giúp cookie hoạt động trên các request cross-site
+        expires: 7, // Cookie hết hạn sau 7 ngày
+      });
     },
     onLogout: (state) => {
       state.currentUser = null;
